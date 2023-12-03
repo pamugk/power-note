a82752006d1d166eb3c6aa456ee33ac7d88e80d7
Alexander Permyakov <pamugk@gmail.com>
2023-12-03 11:37:38 +0500

    UI layout
    added scaffold for future adaptive layout implementation

--- src/main/kotlin/ui/App.kt
+++ src/main/kotlin/ui/App.kt
@@ -7,9 +7,7 @@ import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Archive
 import androidx.compose.material.icons.outlined.Lightbulb
-import androidx.compose.material3.NavigationDrawerItem
-import androidx.compose.material3.PermanentDrawerSheet
-import androidx.compose.material3.PermanentNavigationDrawer
+import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -18,44 +16,47 @@ import navigation.Location
 import ui.pages.ArchivePage
 import ui.pages.NotesPage
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 @Preview
 fun App() {
-    val archivedNotes = mutableStateListOf<Note>()
-    val notes = mutableStateListOf<Note>()
-
     var location by remember { mutableStateOf(Location.MAIN) }
     var viewedArchivedNote by remember { mutableStateOf<Note?>(null) }
     var viewedNote by remember { mutableStateOf<Note?>(null) }
 
+    val archivedNotes = mutableStateListOf<Note>()
+    val notes = mutableStateListOf<Note>()
+
     AppTheme {
-        PermanentNavigationDrawer(
-            drawerContent = {
-                PermanentDrawerSheet(Modifier.width(IntrinsicSize.Min)) {
-                    Spacer(Modifier.height(12.dp))
-                    NavigationDrawerItem(
-                        label = { Text("Заметки") },
-                        selected = location == Location.MAIN,
-                        onClick = { location = Location.MAIN },
-                        icon = { Icon(Icons.Outlined.Lightbulb, "Заметки") }
+        Scaffold() { innerPadding ->
+            PermanentNavigationDrawer(
+                drawerContent = {
+                    PermanentDrawerSheet(Modifier.width(IntrinsicSize.Min)) {
+                        Spacer(Modifier.height(12.dp))
+                        NavigationDrawerItem(
+                            label = { Text("Заметки") },
+                            selected = location == Location.MAIN,
+                            onClick = { location = Location.MAIN },
+                            icon = { Icon(Icons.Outlined.Lightbulb, "Заметки") }
+                        )
+                        NavigationDrawerItem(
+                            label = { Text("Архив") },
+                            selected = location == Location.ARCHIVE,
+                            onClick = { location = Location.ARCHIVE },
+                            icon = { Icon(Icons.Outlined.Archive, "Архив") }
+                        )
+                    }
+                },
+                modifier = Modifier.fillMaxHeight().padding(innerPadding),
+            ) {
+                when (location) {
+                    Location.MAIN -> NotesPage(
+                        modifier = Modifier.fillMaxSize(),
                     )
-                    NavigationDrawerItem(
-                        label = { Text("Архив") },
-                        selected = location == Location.ARCHIVE,
-                        onClick = { location = Location.ARCHIVE },
-                        icon = { Icon(Icons.Outlined.Archive, "Архив") }
+                    Location.ARCHIVE -> ArchivePage(
+                        modifier = Modifier.fillMaxSize(),
                     )
                 }
-            },
-            modifier = Modifier.fillMaxHeight(),
-        ) {
-            when (location) {
-                Location.MAIN -> NotesPage(
-                    modifier = Modifier.fillMaxSize(),
-                )
-                Location.ARCHIVE -> ArchivePage(
-                    modifier = Modifier.fillMaxSize(),
-                )
             }
         }
     }
