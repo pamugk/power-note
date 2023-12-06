eb111074cc34206df3006ba6a08c4a3c528e6eb9
Alexander Permyakov <pamugk@gmail.com>
2023-12-06 09:58:45 +0500

    Data storage
    - updated data path to match recommended ways of some OSes

--- src/main/kotlin/environment/Environment.kt
+++ src/main/kotlin/environment/Environment.kt
@@ -4,12 +4,20 @@ import java.nio.file.Files
 import kotlin.io.path.Path
 
 private const val APP_ID = "com.github.pamugk.PowerNote"
+private const val APP_NAME = "PowerNote"
+
+private fun getDefaultDataPath(s: String): String =
+    when (System.getProperty("os.name")) {
+        "Linux" -> "${System.getProperty("user.home")}$s.local${s}share$s${APP_NAME.lowercase()}$s"
+        "Mac", "Mac OS X" -> "${System.getProperty("user.home")}${s}Library$s$APP_NAME$s"
+        else -> "${System.getProperty("user.home")}$s.${APP_NAME.lowercase()}$s"
+    }
 
 fun getDataPath(): String {
     val s = System.getProperty("file.separator")
-    val path = System.getProperty("XDG_DATA_HOME")?.let { dataPath ->
+    val path = System.getenv("XDG_DATA_HOME")?.let { dataPath ->
         "$dataPath$s$APP_ID$s"
-    } ?: "${System.getProperty("user.home")}$s.powernote$s"
+    } ?: getDefaultDataPath(s)
     Files.createDirectories(Path(path))
     return path
 }
