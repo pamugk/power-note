09ef781919e260b7741feef25edd4003f9efe8d2
Alexander Permyakov <pamugk@gmail.com>
2023-12-10 11:42:13 +0500

    Packaging
    - temporarily replaced logger to fix release packaging

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,19 +15,15 @@ repositories {
 
 val exposedVersion: String by project
 dependencies {
-    // Note, if you develop a library, you should use compose.desktop.common.
-    // compose.desktop.currentOs should be used in launcher-sourceSet
-    // (in a separate module for demo project and in testMain).
-    // With compose.desktop.common you will also lose @Preview functionality
     implementation(compose.desktop.currentOs)
     implementation(compose.materialIconsExtended)
     implementation(compose.material3)
-    implementation("ch.qos.logback:logback-classic:1.4.14")
     implementation("com.mohamedrejeb.richeditor:richeditor-compose:1.0.0-beta05")
     implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
     implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
     implementation("org.jetbrains.exposed:exposed-kotlin-datetime:$exposedVersion")
     implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.5.0")
+    implementation("org.slf4j:slf4j-nop:2.0.9")
     implementation("org.xerial:sqlite-jdbc:3.44.1.0")
 }
 
--- src/main/kotlin/environment/Environment.kt
+++ src/main/kotlin/environment/Environment.kt
@@ -1,23 +1,26 @@
 package environment
 
+import java.nio.file.FileSystems
 import java.nio.file.Files
 import kotlin.io.path.Path
 
 private const val APP_ID = "com.github.pamugk.PowerNote"
 private const val APP_NAME = "PowerNote"
 
-private fun getDefaultDataPath(s: String): String =
-    when (System.getProperty("os.name")) {
+private fun getDefaultDataPath(): String {
+    val s = FileSystems.getDefault().separator
+    return when (System.getProperty("os.name")) {
         "Linux" -> "${System.getProperty("user.home")}$s.local${s}share$s${APP_NAME.lowercase()}$s"
         "Mac", "Mac OS X" -> "${System.getProperty("user.home")}${s}Library$s$APP_NAME$s"
         else -> "${System.getProperty("user.home")}$s.${APP_NAME.lowercase()}$s"
     }
+}
 
 fun getDataPath(): String {
-    val s = System.getProperty("file.separator")
+    val s = FileSystems.getDefault().separator
     val path = System.getenv("XDG_DATA_HOME")?.let { dataPath ->
         "$dataPath$s$APP_ID$s"
-    } ?: getDefaultDataPath(s)
+    } ?: getDefaultDataPath()
     Files.createDirectories(Path(path))
     return path
 }
